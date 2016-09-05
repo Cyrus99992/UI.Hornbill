a33062d8ccf762096d97bdad8d446135d7b128a2
Cyrus99992 <cyrus99@126.com>
2016-09-05 18:41:39 +0800

    0905 login page powerup

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -1,6 +1,6 @@
 /*
- *      This is a demo file used only for the main dashboard (index.html)
- **/
+ *	主控台仪表板JS
+ */
 "use strict";
 
 $(function () {
@@ -24,7 +24,7 @@ $(function () {
   });
 
   //bootstrap WYSIHTML5 - text editor
-  $(".textarea").wysihtml5();
+  //$(".textarea").wysihtml5();
 
   $('.daterange').daterangepicker(
           {
@@ -45,46 +45,6 @@ $(function () {
 
   /* jQueryKnob */
   $(".knob").knob();
-
-  //jvectormap data
-  var visitorsData = {
-    "US": 398, //USA
-    "SA": 400, //Saudi Arabia
-    "CA": 1000, //Canada
-    "DE": 500, //Germany
-    "FR": 760, //France
-    "CN": 300, //China
-    "AU": 700, //Australia
-    "BR": 600, //Brazil
-    "IN": 800, //India
-    "GB": 320, //Great Britain
-    "RU": 3000 //Russia
-  };
-  //World map by jvectormap
-  $('#world-map').vectorMap({
-    map: 'world_mill_en',
-    backgroundColor: "transparent",
-    regionStyle: {
-      initial: {
-        fill: '#e4e4e4',
-        "fill-opacity": 1,
-        stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
-      }
-    },
-    series: {
-      regions: [{
-          values: visitorsData,
-          scale: ["#92c1dc", "#ebf4f9"],
-          normalizeFunction: 'polynomial'
-        }]
-    },
-    onRegionLabelShow: function (e, el, code) {
-      if (typeof visitorsData[code] != "undefined")
-        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
-    }
-  });
 
   //Sparkline charts
   var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
